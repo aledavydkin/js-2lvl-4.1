f62d0fbcc93dc023416a3502d3ac6dbd3dbad4d5
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-05-13 23:57:06 +0500

    Сделал через throw

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
 export default function validateNumber(number) {
-  try {
-    const convertNum = parseInt(number, 10);
-    if (Number.isNaN(convertNum)) {
-      throw new Error('Допускается ввод только чисел в десятичной системе счисления');
-    }
-    if (convertNum !== number) {
-      throw new Error('Допускается ввод только чисел в десятичной системе счисления, цифры от 0 до 9');
-    }
-    return convertNum;
-  } catch (e) {
-    return e;
+  const convertNum = parseInt(number, 10);
+  if (Number.isNaN(convertNum)) {
+    throw new Error('Допускается ввод только чисел в десятичной системе счисления');
   }
+  if (convertNum !== number) {
+    throw new Error('Допускается ввод только чисел в десятичной системе счисления, цифры от 0 до 9');
+  }
+  return convertNum;
 }
+
