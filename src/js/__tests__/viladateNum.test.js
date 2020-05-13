90ec1b437b773af4cee01ab03368872b1b7e5a15
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-05-13 23:58:36 +0500

    Сделал через throw

--- src/js/__tests__/viladateNum.test.js
+++ src/js/__tests__/viladateNum.test.js
@@ -6,7 +6,7 @@ import validateNumber from '../app';
 describe('Проверка на ввод не является числом в десятичной системе счисления', () => {
   test('Если пришло число', () => {
     const result = validateNumber(1);
-    expect(result).toEqual(1);
+    expect(result).toBe(1);
   });
 
   test('Если пришло не число', () => {
--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,3 @@ export default function validateNumber(number) {
   }
   return convertNum;
 }
-
