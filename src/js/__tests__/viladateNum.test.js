3278cac3c534433a380785b4a54c1aadd1c3e315
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-05-13 13:15:45 +0500

    Поправил тесты

--- src/js/__tests__/viladateNum.test.js
+++ src/js/__tests__/viladateNum.test.js
@@ -6,16 +6,17 @@ import validateNumber from '../app';
 describe('Проверка на ввод не является числом в десятичной системе счисления', () => {
   test('Если пришло число', () => {
     const result = validateNumber(1);
-    expect(result).toBe(1);
+    expect(result).toEqual(1);
   });
 
   test('Если пришло не число', () => {
     const result = validateNumber('Не число');
-    expect(() => result()).toThrow();
+    expect(result).toEqual(Error('Допускается ввод только чисел в десятичной системе счислен'
+      + 'ия'));
   });
 
   test('Если пришло число не в дясятичной системе счисления ', () => {
     const result = validateNumber('-0XF');
-    expect(() => result()).toThrow();
+    expect(result).toEqual(Error('Допускается ввод только чисел в десятичной системе счисления, цифры от 0 до 9'));
   });
 });
